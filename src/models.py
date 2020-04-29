remove gumbel from computation graph; fix tf_prob bug;

src/models.py
@@ -166,15 +166,21 @@ class Decoder(nn.Module):
                 # Teacher forcing
                 teacher_forcing_prob = get_tf_prob(loss)
                 if (i!=0 and random.random() <= teacher_forcing_prob):
+                    if gumbel_noise:
+                        prediction = get_gumbel_prediction(prediction)
                     char_embed = self.embedding(prediction.argmax(dim=-1))
                 else:
                     char_embed = embeddings[:,i,:]
             else:
                 if i!=0 and random_search:
+                    if gumbel_noise:
+                        prediction = get_gumbel_prediction(prediction)
                     selected_char = torch.distributions.Categorical(nn.functional.softmax(prediction, dim=-1)).sample()
                 elif i == 0:
                     selected_char = torch.zeros(batch_size, dtype=torch.long).fill_(sos_idx).to(DEVICE)
                 else:
+                    if gumbel_noise:
+                        prediction = get_gumbel_prediction(prediction)
                     selected_char = prediction.argmax(dim=-1)
                 char_embed = self.embedding(selected_char)
             # char_embed.shape: [batch_size, hidden_dim]
@@ -207,9 +213,6 @@ class Decoder(nn.Module):
 
             prediction = self.character_prob(torch.cat([output, context], dim=1))
             # prediction.shape: [batch_size, vocab_size]
-
-            if gumbel_noise:
-                prediction = get_gumbel_prediction(prediction)
 
             predictions.append(prediction.unsqueeze(1))
 
@@ -245,24 +248,26 @@ class Seq2Seq(nn.Module):
         return predictions
 
 def get_gumbel_prediction(prediction):
+    prediction = prediction.detach()
     U = torch.rand(prediction.shape[1]).to(DEVICE)
     G = -torch.log(-torch.log(U))
     prediction = torch.log(torch.nn.functional.softmax(prediction, dim=-1)) + G.repeat(prediction.shape[0], 1)
     return prediction
 
+# TODO: modify threshold
 def get_tf_prob(loss):
     prob = 0.0
     if (loss==-1):
         prob = 0.1
-    elif (loss > 25):
-        prob = 0.0
-    elif (loss > 20):
-        prob = 0.05
-    elif (loss > 10):
-        prob = 0.10
     elif (loss > 5):
-        prob = 0.20
+        prob = 0.0
     elif (loss > 2.5):
+        prob = 0.05
+    elif (loss > 2.0):
+        prob = 0.10
+    elif (loss > 1.5):
+        prob = 0.20
+    elif (loss > 1.0):
         prob = 0.30
     else:
         prob = 0.40